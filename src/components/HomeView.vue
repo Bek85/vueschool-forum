4ae7b4eb05f71bac806f038478fe7095a9529ffa
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-06 09:49:14 +0500

    display a list of elements in vue template directly

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,15 +1,29 @@
 <script>
+import sourceData from "@/data.json";
 export default {
   name: "HomeView",
   data() {
-    return {};
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
   },
 };
 </script>
 
 
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>
+        {{
+          users.find((u) => u.id === posts.find((p) => p.id === postId).userId)
+            .username
+        }}
+      </p>
+      <p>{{ posts.find((p) => p.id === postId).text }}</p>
+    </div>
   </div>
 </template>
