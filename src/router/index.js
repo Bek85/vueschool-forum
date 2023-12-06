d0e6f95d193b28d4c8b76ba523a1525410659862
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-06 19:55:26 +0500

    Add thread existence check in beforeEnter hook

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import sourceData from '@/data.json';
 
 const routes = [
   {
@@ -11,6 +12,26 @@ const routes = [
     name: 'thread',
     component: () => import('@/views/ThreadView.vue'),
     props: true,
+    beforeEnter: (to, from, next) => {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
+      // if exists continue
+      if (threadExists) {
+        return next();
+      } else {
+        // if doesn't exist redirect to not-found
+        next({
+          name: 'not-found',
+          params: {
+            pathMatch: to.path.substring(1).split('/'),
+          },
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+    },
   },
   {
     path: '/:pathMatch(.*)*',
