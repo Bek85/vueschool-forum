b33b704bff9c5445602e42fdf82dddd4dd6972ff
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-06 10:28:00 +0500

    setup eslint

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), eslintPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
