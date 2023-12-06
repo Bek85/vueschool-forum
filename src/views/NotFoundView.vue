0df493959cfc9715a9dcd52341cb8bcdfee93b34
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-06 19:24:42 +0500

    update text on not found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -2,7 +2,7 @@
   <div class="not-found">
     <h1>404 Not Found</h1>
     <p>The page you are looking for does not exist.</p>
-    <router-link to="/">Go back to home page</router-link>
+    <router-link to="{name: 'home'}">Read some cool threads</router-link>
   </div>
 </template>
 
