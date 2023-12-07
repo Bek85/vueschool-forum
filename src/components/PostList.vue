a9ae708abbd9f33b79b75afb82c1b183fac6f732
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-07 14:36:53 +0500

    Refactor PostList component to use AppDate component

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,12 @@
 <script>
 import sourceData from '@/data.json';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import localizedDate from 'dayjs/plugin/localizedFormat';
-dayjs.extend(relativeTime);
-dayjs.extend(localizedDate);
+import AppDate from '@/components/AppDate.vue';
 
 export default {
   name: 'PostList',
+  components: {
+    AppDate,
+  },
   props: {
     posts: {
       type: Array,
@@ -22,12 +21,6 @@ export default {
   methods: {
     userById(userId) {
       return this.users.find((u) => u.id === userId);
-    },
-    diffForHumans(timestamp) {
-      return dayjs.unix(timestamp).fromNow();
-    },
-    humanFriendlyDate(timestamp) {
-      return dayjs.unix(timestamp).format('llll');
     },
   },
 };
@@ -55,11 +48,8 @@ export default {
           </p>
         </div>
       </div>
-      <div
-        class="post-date text-faded"
-        :title="humanFriendlyDate(post.publishedAt)"
-      >
-        {{ diffForHumans(post.publishedAt) }}
+      <div class="post-date text-faded">
+        <AppDate :timestamp="post.publishedAt" />
       </div>
     </div>
   </div>
