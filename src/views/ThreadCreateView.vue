cd85df23d30102ecd84d251214b4cfa0ca42c2fc
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-16 16:22:39 +0500

    create new forum thread page component

---
+++ src/views/ThreadCreateView.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+  props: {
+    forum: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      title: '',
+      text: '',
+    };
+  },
+  methods: {
+    // dispatch vue action
+    save() {
+      this.$store.dispatch('createThread', {
+        title: this.title,
+        text: this.text,
+        forumId: this.forum.id,
+      });
+    },
+  },
+};
+</script>
+
+
+<template>
+  <div class="col-full push-top">
+    <h1>
+      Create new thread in <i>{{ forum.name }}</i>
+    </h1>
+
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <label for="thread_title">Title:</label>
+        <input
+          id="thread_title"
+          v-model="title"
+          type="text"
+          class="form-input"
+          name="title"
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea
+          id="thread_content"
+          v-model="text"
+          class="form-input"
+          name="content"
+          rows="8"
+          cols="140"
+        ></textarea>
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-ghost">Cancel</button>
+        <button class="btn btn-blue" type="submit" name="Publish">
+          Publish
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
