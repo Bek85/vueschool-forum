46f27f9acc498d3bb29c53611c9463f20cfb8830
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-18 10:29:52 +0500

    create <ThreadEditor /> component and refactor the existing code to accordingly

---
+++ src/components/ThreadEditor.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  props: {
+    forumId: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['save', 'cancel'],
+  data() {
+    return {
+      title: '',
+      text: '',
+    };
+  },
+  methods: {
+    save() {
+      this.$emit('save', { title: this.title, text: this.text });
+    },
+  },
+};
+</script>
+
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input
+        id="thread_title"
+        v-model="title"
+        type="text"
+        class="form-input"
+        name="title"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="thread_content">Content:</label>
+      <textarea
+        id="thread_content"
+        v-model="text"
+        class="form-input"
+        name="content"
+        rows="8"
+        cols="140"
+      ></textarea>
+    </div>
+
+    <div class="btn-group">
+      <button class="btn btn-ghost" @click.prevent="$emit('cancel')">
+        Cancel
+      </button>
+      <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
+    </div>
+  </form>
+</template>
--- src/views/ThreadCreateView.vue
+++ src/views/ThreadCreateView.vue
@@ -6,12 +6,6 @@ export default {
       required: true,
     },
   },
-  data() {
-    return {
-      title: '',
-      text: '',
-    };
-  },
   computed: {
     forum() {
       return this.$store.state.forums.find(
@@ -21,10 +15,10 @@ export default {
   },
   methods: {
     // dispatch vue action
-    async save() {
+    async save({ title, text }) {
       const thread = await this.$store.dispatch('createThread', {
-        title: this.title,
-        text: this.text,
+        title,
+        text,
         forumId: this.forum.id,
       });
       this.$router.push({
@@ -53,36 +47,6 @@ export default {
       Create new thread in <i>{{ forum.name }}</i>
     </h1>
 
-    <form @submit.prevent="save">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input
-          id="thread_title"
-          v-model="title"
-          type="text"
-          class="form-input"
-          name="title"
-        />
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          id="thread_content"
-          v-model="text"
-          class="form-input"
-          name="content"
-          rows="8"
-          cols="140"
-        ></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button class="btn btn-ghost" @click="cancel">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">
-          Publish
-        </button>
-      </div>
-    </form>
+    <ThreadEditor :forum-id="forum.id" @save="save" @cancel="cancel" />
   </div>
 </template>
