aa0294aac65ab5550910841a05adf4e481337d40
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-22 15:04:50 +0500

    store new post in Cloud Firestore with Vuex

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase';
+import firebase from '@/helpers/firebase';
 import { findById, randomHex } from '@/helpers';
 
 export default {
@@ -48,15 +48,28 @@ export default {
   fetchPosts: ({ dispatch }, { ids }) =>
     dispatch('fetchItems', { resource: 'posts', ids, emoji: '💭' }),
 
-  createPost: ({ commit, state }, post) => {
+  createPost: async ({ commit, state }, post) => {
     post.id = randomHex(10);
     post.userId = state.authId;
     post.publishedAt = Math.floor(Date.now() / 1000);
-    commit('setPost', { post });
+    const newPost = await firebase.firestore().collection('posts').add(post);
+
+    await firebase
+      .firestore()
+      .collection('threads')
+      .doc(post.threadId)
+      .update({
+        posts: firebase.firestore.FieldValue.arrayUnion(newPost.id),
+        contributors: firebase.firestore.FieldValue.arrayUnion(state.authId),
+      });
+
+    commit('setItem', { resource: 'posts', item: { ...post, id: newPost.id } });
+
     commit('appendPostToThread', {
-      childId: post.id,
+      childId: newPost.id,
       parentId: post.threadId,
     });
+
     commit('appendContributorToThread', {
       childId: state.authId,
       parentId: post.threadId,
