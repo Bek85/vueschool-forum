e46e135826658a362900571dde28aef1c9ef3d25
Bekzod Akbarov <stx.webdev@gmail.com>
2024-01-24 18:11:14 +0500

    use <AppFormField /> component in <UserProfileCardEditor /> component

--- src/components/UserProfileCardEditor.vue
+++ src/components/UserProfileCardEditor.vue
@@ -51,7 +51,7 @@ export default {
 
 <template>
   <div class="profile-card">
-    <form @submit.prevent="save">
+    <VeeForm @submit="save">
       <p class="text-center avatar-edit">
         <label for="avatar">
           <AppAvatarImg
@@ -80,33 +80,24 @@ export default {
       </p>
       <UserProfileCardEditorRandomAvatar @hit="currentUser.avatar = $event" />
 
-      <div class="form-group">
-        <input
-          v-model="currentUser.username"
-          type="text"
-          placeholder="Username"
-          class="form-input text-lead text-bold"
-        />
-      </div>
+      <AppFormField
+        v-model="currentUser.username"
+        name="username"
+        label="Username"
+      />
 
-      <div class="form-group">
-        <input
-          v-model="currentUser.name"
-          type="text"
-          placeholder="Full Name"
-          class="form-input text-lead"
-        />
-      </div>
+      <AppFormField
+        v-model="currentUser.name"
+        name="username"
+        label="Full Name"
+      />
 
-      <div class="form-group">
-        <label for="user_bio">Bio</label>
-        <textarea
-          id="user_bio"
-          v-model="currentUser.bio"
-          class="form-input"
-          placeholder="Write a few words about yourself."
-        ></textarea>
-      </div>
+      <AppFormField
+        v-model="currentUser.bio"
+        as="textarea"
+        name="bio"
+        label="Bio"
+      />
 
       <div class="stats">
         <span>{{ user.postsCount }} posts</span>
@@ -115,40 +106,31 @@ export default {
 
       <hr />
 
-      <div class="form-group">
-        <label class="form-label" for="user_website">Website</label>
-        <input
-          id="user_website"
-          v-model="currentUser.website"
-          autocomplete="off"
-          class="form-input"
-        />
-      </div>
+      <AppFormField
+        v-model="currentUser.website"
+        name="website"
+        label="Website"
+        autocomplete="off"
+      />
 
-      <div class="form-group">
-        <label class="form-label" for="user_email">Email</label>
-        <input
-          id="user_email"
-          v-model="currentUser.email"
-          autocomplete="off"
-          class="form-input"
-        />
-      </div>
+      <AppFormField
+        v-model="currentUser.email"
+        name="email"
+        label="Email"
+        autocomplete="off"
+      />
 
-      <div class="form-group">
-        <label class="form-label" for="user_location">Location</label>
-        <input
-          id="user_location"
-          v-model="currentUser.location"
-          autocomplete="off"
-          class="form-input"
-        />
-      </div>
+      <AppFormField
+        v-model="currentUser.location"
+        name="location"
+        label="Location"
+        autocomplete="off"
+      />
 
       <div class="btn-group space-between">
         <button class="btn-ghost" @click.prevent="cancel">Cancel</button>
         <button type="submit" class="btn-blue">Save</button>
       </div>
-    </form>
+    </VeeForm>
   </div>
 </template>
