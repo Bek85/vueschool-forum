e75815e643ff9505d430a3aae21266e809692c3a
Bekzod Akbarov <stx.webdev@gmail.com>
2024-01-24 17:00:49 +0500

    create reusable <AppFormField /> component

---
+++ src/components/AppFormField.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="form-group">
+    <label :for="name">{{ label }}</label>
+    <VeeField
+      :id="name"
+      :model-value="modelValue"
+      :name="name"
+      :label="label"
+      class="form-input"
+      v-bind="$attrs"
+      @input="$emit('modelUpdate', $event)"
+    />
+    <VeeErrorMessage :name="name" class="form-error" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { type: String, required: true },
+    label: { type: String, required: true },
+    modelValue: { type: String, required: true },
+  },
+};
+</script>
